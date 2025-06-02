16779e084902ae588e83c7215a23101ed0e0516b
Neo Adrian <134072533+MaiTheHao@users.noreply.github.com>
2025-06-03 04:16:24 +0700

    Refactor các component AppHeader và AppFooter để cải thiện cấu trúc và style; thêm reset CSS để đồng bộ giao diện toàn ứng dụng.

--- client/src/components/AppFooter/AppFooter.jsx
+++ client/src/components/AppFooter/AppFooter.jsx
@@ -4,21 +4,26 @@ import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import { faFacebook, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import './AppFooter.css';
 
-
 function AppFooter() {
 	return (
-	<div class='app-container' id='app-footer'>
-		<div class='app-block' id='footer-block'>
-			<ul className='footer-list'>
-				<li className='footer-title'>
-					<FontAwesomeIcon icon={faEnvelope} /><a href='https://www.mail.com'>techsquad.it.iuh@gmail.com</a></li>
-				<li className='footer-title'>
-					<FontAwesomeIcon icon={faFacebook} /><a href='https://www.twitter.com'>facebook.com/bancongnghe.it.iuh</a></li>
-				<li className='footer-title'>
-					<FontAwesomeIcon icon={faYoutube} /><a href='https://www.instagram.com'>youtube.com/@bancongnghe.it.iuh</a></li>
-			</ul>
+		<div className='app-container' id='app-footer'>
+			<div className='app-block' id='footer-block'>
+				<ul className='app-footer__list'>
+					<li className='app-footer__item'>
+						<FontAwesomeIcon icon={faEnvelope} />
+						<a href='https://www.mail.com'>techsquad.it.iuh@gmail.com</a>
+					</li>
+					<li className='app-footer__item'>
+						<FontAwesomeIcon icon={faFacebook} />
+						<a href='https://www.twitter.com'>facebook.com/bancongnghe.it.iuh</a>
+					</li>
+					<li className='app-footer__item'>
+						<FontAwesomeIcon icon={faYoutube} />
+						<a href='https://www.instagram.com'>youtube.com/@bancongnghe.it.iuh</a>
+					</li>
+				</ul>
+			</div>
 		</div>
-	</div>
 	);
 }
 
