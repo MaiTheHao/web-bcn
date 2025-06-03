b519b11e3c07e7a8527e00a1c3359be956dffb02
Neo Adrian <134072533+MaiTheHao@users.noreply.github.com>
2025-06-04 02:24:34 +0700

    Thêm logic simple cho pagination Tiếp tục hoàn thiện style - logic

--- client/src/components/ProjectCart/ProjectCart.jsx
+++ client/src/components/ProjectCart/ProjectCart.jsx
@@ -1,35 +1,79 @@
-import './ProjectCart.css'
+import './ProjectCart.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft , faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faAnglesLeft, faAnglesRight } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+import { calcPagination } from '../../styles/utils/getPagination';
+
+const LIMIT = 8;
+const MOCK_TOTAL = 100; // Giả sử tổng số phần tử là 100
 function ProjectCart() {
-    const items = [1, 2, 3, 4, 5, 6, 7, 8];
-    return (
-        <>
-            <div className="project">
-                <h1 className='title'>Project</h1>
-                <div className='project-cart'>
-                    <ul className='project-list'>
-                        {
-                            items.map((e, idx) => (
-                                <li className='project-card' key={idx}></li>
-                            ))
-                        }
-                    </ul>
-                    <div className='pagination'>
-                        <button className='pagination-button pagination-button-prev'>
-                            <FontAwesomeIcon icon={faArrowLeft} />
-                        </button>
-                        <button className='pagination-button pagination-number active'>1</button>
-                        <button className='pagination-button pagination-number'>2</button>
-                        <button className='pagination-button pagination-number'>3</button>
-                        <button className='pagination-button pagination-dots'>...</button>
-                        <button className='pagination-button pagination-button-next'>
-                            <FontAwesomeIcon icon={faArrowRight} />
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+	const [pagination, setPagination] = useState(calcPagination(1, LIMIT, MOCK_TOTAL));
+	const handleChangePage = (page) => {
+		if (page >= 1 && page <= pagination.totalPage) {
+			setPagination(calcPagination(page, LIMIT, MOCK_TOTAL));
+		}
+	};
+
+	const handleNextPage = () => {
+		handleChangePage(pagination.page + 1);
+	};
+
+	const handlePrevPage = () => {
+		handleChangePage(pagination.page - 1);
+	};
+
+	const isActivePage = (page) => {
+		return pagination.page === page ? 'active' : '';
+	};
+
+	const items = [1, 2, 3, 4, 5, 6, 7, 8];
+	return (
+		<>
+			<div className='project'>
+				<h1 className='title'>Project</h1>
+				<div className='project-cart'>
+					<ul className='project-list'>
+						{items.map((e, idx) => (
+							<li className='project-card' key={idx}></li>
+						))}
+					</ul>
+					<div className='pagination'>
+						<button className='pagination-button pagination-button-prev' onClick={handlePrevPage}>
+							<FontAwesomeIcon icon={faAnglesLeft} />
+						</button>
+						<button
+							className={`pagination-button pagination-number ${isActivePage(1)}`}
+							onClick={() => handleChangePage(1)}
+						>
+							1
+						</button>
+
+						{/* <button className={`pagination-button pagination-number ${isActivePage(2) && 'active'}`}>
+							2
+						</button>
+						<button className={`pagination-button pagination-number ${isActivePage(3) && 'active'}`}>
+							3
+						</button> */}
+
+						<span className='pagination-dots'>...</span>
+						<button
+							className={`pagination-button pagination-number ${isActivePage(pagination.totalPage)}`}
+							onClick={() => handleChangePage(pagination.totalPage)}
+						>
+							{pagination.totalPage}
+						</button>
+						<button className='pagination-button pagination-button-next' onClick={handleNextPage}>
+							<FontAwesomeIcon icon={faAnglesRight} />
+						</button>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
+
+/*
+icon pagination gì kì dị ba, không giống design tí nào :_)))
+*/
+
 export default ProjectCart;
---
+++ client/src/styles/utils/getPagination.js
@@ -0,0 +1,19 @@
+/**
+ * @function calcPagination
+ * @description Tính toán phân trang
+ * @param {number} page - idx trang hiện tại
+ * @param {number} limit - Số lượng phần tử trên mỗi trang
+ * @param {number} total - Tổng số phần tử
+ * @returns {object} Thông tin phân trang page, limit, total, totalPage
+ */
+export function calcPagination(page, limit, total) {
+	const totalPage = Math.ceil(total / limit);
+	const pagination = {
+		page: page,
+		limit: limit,
+		total: total,
+		totalPage: totalPage,
+	};
+
+	return pagination;
+}
