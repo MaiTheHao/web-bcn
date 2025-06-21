d2dc00238abc82a2970b1cc50f19b7a2964d01b4
takiindev <takin2k5@gmail.com>
2025-06-21 22:30:34 +0700

    hoan thien Select components

--- client/src/components/ItemTable/ItemTable.jsx
+++ client/src/components/ItemTable/ItemTable.jsx
@@ -1,10 +1,11 @@
 import './ItemTable.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAnglesLeft, faAnglesRight } from '@fortawesome/free-solid-svg-icons';
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useEffect } from 'react';
 import { calcPagination } from '../../styles/utils/getPagination';
 import DropDownIcon from '/svg/DropDown.svg';
 import SearchIcon from '/svg/Search.svg';
+import Select from '../Select/Select';
 
 
 function ItemTable({
@@ -53,49 +54,43 @@ function ItemTable({
 
 	const pageNumbers = getPageNumbers();
 
-	const [selection1, setSelection1] = useState(filterFields[0]?.placeholder || "...");
-	const [selection2, setSelection2] = useState(filterFields[1]?.placeholder || "...");
-	const handleSelection1Change = (e) => {
-		setSelection1(e.target.value);
+	const [filterCriteria, setFilterCriteria] = useState([]);
+	// useEffect(() => {
+	// 	console.log('Filter criteria updated:', filterCriteria);
+	// }, [filterCriteria]);
+	const handle = (fieldName, value) => {
+		const newFilterState = filterCriteria.filter((c) => c.fieldName !== fieldName);
+		newFilterState.push({ fieldName, value });
+		setFilterCriteria(newFilterState);
 	};
 
-	const handleSelection2Change = (e) => {
-		setSelection2(e.target.value);
-	};
+
+
 	return (
-		<div className="itemTable_root">
-			<h1 className="itemTable_title">{title}</h1>
-			<div className="itemTable_filterContainer">
-				<div className="itemTable_filterItem">
-					<h2>{selection1}</h2>
-					<img src={DropDownIcon} alt="Dropdown Icon" className="dropdown-icon" />
-					<select onChange={handleSelection1Change} className="itemTable_select">
-						<option value={filterFields[0]?.placeholder}>{filterFields[0]?.placeholder}</option>
-						{filterFields[0]?.options?.map((option, index) => (
-							<option key={`${option.value}-${option.label}-${index}`} value={option.value}>{option.label}</option>
-						))}
-					</select>
-				</div>
-				<div className="itemTable_filterItem">
-					<h2>{selection2}</h2>
-					<img src={DropDownIcon} alt="Dropdown Icon" className="dropdown-icon" />
-					<select onChange={handleSelection2Change} className="itemTable_select">
-						<option value={filterFields[1]?.placeholder}>{filterFields[1]?.placeholder}</option>
-						{filterFields[1]?.options?.map((option, index) => (
-							<option key={`${option.value}-${option.label}-${index}`} value={option.value}>{option.label}</option>
-						))}
-					</select>
-				</div>
-				<div className="itemTable_filterItem">
-					<img src={SearchIcon} alt="Search Icon" className="search-icon" />
-					<input type="text" className="itemTable_searchInput" placeholder="Nhập từ khóa cần tìm kiếm" />
+		<div className="item-table">
+			<h1 className="item-table-title">{title}</h1>
+			<div className="item-table-filter-container">
+				{filterFields.map((field, idx) => (
+					<div className="item-table-filter-item" key={field.fieldName}>
+						<Select
+							options={field.options}
+							placeholder={field.placeholder}
+							onSelect={(value) => handle(field.fieldName, value)}
+							selectedValue={filterCriteria.find(f => f.fieldName === field.fieldName)?.value || null}
+							customClassName={{ select: 'select-filter-item', trigger: 'item-table-select' }}
+						/>
+					</div>
+				))}
+				<div className="item-table-filter-item">
+					<img src={SearchIcon} alt="Search Icon" className="item-table-search-icon" />
+					<input type="text" className="item-table-search-input" placeholder="Nhập từ khóa cần tìm kiếm" />
 				</div>
 			</div>
-			<div className="itemTable_cart">
-				<ul className="itemTable_list" style={{ display: 'flex', flexWrap: 'wrap' }}>
+			<div className="item-table-cart">
+				<ul className="item-table-list" style={{ display: 'flex', flexWrap: 'wrap' }}>
 					{currentItems.map((item, index) => (
 						<li
-							className="itemTable_card"
+							className="item-table-card"
 							key={index}
 							style={{ width: `calc( (100% - ${20 * (itemsPerRow - 1)}px) / ${itemsPerRow})` }}
 						>
@@ -104,9 +99,9 @@ function ItemTable({
 					))}
 				</ul>
 				{pagination.totalPage > 1 && (
-					<div className="itemTable_pagination">
+					<div className="item-table-pagination">
 						<button
-							className="itemTable_paginationButton itemTable_paginationButton-prev"
+							className="item-table-pagination-button item-table-pagination-button-prev"
 							onClick={handlePrevPage}
 							disabled={page === 1}
 						>
@@ -114,13 +109,13 @@ function ItemTable({
 						</button>
 						{pageNumbers.map((p, idx) =>
 							p === '...' ? (
-								<span className="pagination-dots" key={idx}>
+								<span className="item-table-pagination-dots" key={idx}>
 									...
 								</span>
 							) : (
 								<button
 									key={p}
-									className={`itemTable_paginationButton itemTable_pagination-number ${isActivePage(p)}`}
+									className={`item-table-pagination-button item-table-pagination-number ${isActivePage(p)}`}
 									onClick={() => handleChangePage(p)}
 								>
 									{p}
@@ -128,7 +123,7 @@ function ItemTable({
 							)
 						)}
 						<button
-							className="itemTable_paginationButton itemTable_paginationButton-next"
+							className="item-table-pagination-button item-table-pagination-button-next"
 							onClick={handleNextPage}
 							disabled={page === pagination.totalPage}
 						>
